b38704c07c3ef2f9ddc10e6c0660571bf279a371
Felipe M <philmes@gmail.com>
2022-01-17 18:23:43 -0300

    Initial commit

---
+++ src/main/java/com/sclw/SclwConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.sclw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SclwConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SclwConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sclw/SclwConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sclw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SclwConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
